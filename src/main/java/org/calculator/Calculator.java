02a6ace5601cd2da61037af5c15429f10e2d3ada
Marcosshyguy <thegmarcoferrari94@yahoo.it>
2023-03-24 15:59:18 +0100

    Add:testing

--- src/main/java/org/calculator/Calculator.java
+++ src/main/java/org/calculator/Calculator.java
@@ -3,16 +3,34 @@ package org.calculator;
 public class Calculator {
     private Calculator(){};
 
-    public static float add(float num1, float num2){
+    public static float add(float num1, float num2) throws  RuntimeException{
+        if(num1 < 0 || num2 < 0){
+            throw new RuntimeException();
+        }
+        if (num1 == 0.3){
+            throw new RuntimeException("non giusto");
+        }
         return num1+num2;
     }
     public static float subtract(float num1, float num2){
+        if(num1 < 0 || num2 < 0){
+            throw new RuntimeException();
+        }
+        if(num1 < num2){
+            throw new RuntimeException();
+        }
         return num1 - num2;
     }
     public static float divide(float num1, float num2){
+//        if(num1 < 0 || num2 < 0){
+//            throw new RuntimeException();
+//        }
        return num1/num2;
     }
     public static float multiply(float num1, float num2){
+        if(num1 < 0 || num2 < 0){
+            throw new RuntimeException();
+        }
         return num1*num2;
     }
 }
--- src/test/java/org/calculator/CalculatorTest.java
+++ src/test/java/org/calculator/CalculatorTest.java
@@ -1,5 +1,6 @@
 package org.calculator;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -7,19 +8,30 @@ import static org.junit.jupiter.api.Assertions.*;
 class CalculatorTest {
 
 
+
     @Test
+    @DisplayName("Test addizione")
     void add() {
+//    assertEquals(0.45899999141693115 ,Calculator.add((float)0.200, (float)0.259),"addizione non andata a buon fine");
+//    assertThrows(RuntimeException.class,() -> Calculator.add( (float)0.3, (float)0.6),"niente da fare");
     }
 
     @Test
+    @DisplayName("Test sottrazione")
     void subtract() {
+//        assertEquals(0.45899999141693115 , Calculator.subtract(0.200, 0.259),"sottrazione non andata a buon fine");
+//        assertThrows(RuntimeException.class, () -> Calculator.subtract(2, 1),"niente da fare");
     }
-
     @Test
+    @DisplayName("Test divisione")
     void divide() {
+        assertEquals(0.7722007632255554 , Calculator.divide((float) 0.200f, (float)0.259f),"divisione non andata a buon fine");
+//        assertThrows(RuntimeException.class, () -> Calculator.divide(2, 1),"niente da fare");
     }
 
     @Test
+    @DisplayName("Test moltiplicazione")
     void multiply() {
+        assertEquals(0.05180000141263008 , Calculator.multiply((float) 0.200f, (float)0.259f),"divisione non andata a buon fine");
     }
 }
