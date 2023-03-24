423c0baa35ec53681ea3d66993a9f40a056ee0f9
Marcosshyguy <thegmarcoferrari94@yahoo.it>
2023-03-24 14:29:52 +0100

    scaffolding

---
+++ src/main/java/org/calculator/Calculator.java
@@ -0,0 +1,18 @@
+package org.calculator;
+
+public class Calculator {
+    private Calculator(){};
+
+    public static float add(float num1, float num2){
+        return num1+num2;
+    }
+    public static float subtract(float num1, float num2){
+        return num1 - num2;
+    }
+    public static float divide(float num1, float num2){
+       return num1/num2;
+    }
+    public static float multiply(float num1, float num2){
+        return num1*num2;
+    }
+}
